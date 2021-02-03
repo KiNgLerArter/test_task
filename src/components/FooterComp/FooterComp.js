55a6630c1315170b16075908ee3a777bbfc94b69
KiNgLerArter <65976256+KiNgLerArter@users.noreply.github.com>
2021-02-03 22:27:49 +0200

    feat(FooterComp): footer

--- src/components/FooterComp/FooterComp.js
+++ src/components/FooterComp/FooterComp.js
@@ -1,10 +1,21 @@
 import classes from './FooterComp.module.css';
 
+import emojiImg from '../../assets/images/emoji.svg';
+
 const footerComp = (props) => (
   <footer
-    className={classes.Footer + ' width_100'}
+    className={classes.Footer + ' width_100 flex_between_center'}
   >
-    
+    <input 
+      type='text'
+      className={classes.Input + ' Fonts_13_16'}
+      placeholder={'Напишите сообщение...'}
+      maxLength={200}
+    />
+    <img 
+      src={emojiImg}
+      className={classes.Emoji}
+    />
   </footer>
 );
 
